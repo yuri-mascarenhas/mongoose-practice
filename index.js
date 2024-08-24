991c811895836f77bf92a5183cb1f29709757dbd
yurism <mascarenhasyuri@hotmail.com>
2024-08-24 17:05:20 -0300

    [Add] Connection with error handling

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 import mongoose from "mongoose";
 
 // MongoDB connection
-const URI = "";
+const URI = "mongodb://localhost:27017/animesApp";
+const DB = mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("Connection started.");
+  })
+  .catch((err) => {
+    console.error("Connection error.");
+    console.error(err);
+  });
